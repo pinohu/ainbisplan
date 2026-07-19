a97d6d1c08bdb91cc5620d1b3d933635a7f9d743
Ikechukwu Ohu, PhD <polycarpohu@gmail.com>
2026-07-19 01:00:15 -0400

    Export archetype registry

---
+++ packages/archetypes/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  archetypeIds,
+  archetypeRegistry,
+  getArchetype,
+  type ArchetypeDefinition,
+  type ArchetypeId,
+  type Density,
+  type MotionLevel,
+  type TrustLevel
+} from "./registry.js";
