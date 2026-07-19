d9f3e8c834754b7c341cf3ced0118a21aa632e1a
Ikechukwu Ohu, PhD <polycarpohu@gmail.com>
2026-07-19 01:01:39 -0400

    Export brand profile generator

---
+++ packages/brand-engine/src/index.ts
@@ -0,0 +1,11 @@
+export {
+  generateBrandProfile,
+  type AudienceSophistication,
+  type BrandInput,
+  type BrandProfile,
+  type BuyingCycle,
+  type EmotionalPosture,
+  type GeographicScope,
+  type ProblemSeverity,
+  type RegulatorySensitivity
+} from "./generateBrand.js";
