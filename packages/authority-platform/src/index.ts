acf03f7a2b9d33a6025afe654531fb0e48174500
Ikechukwu Ohu, PhD <polycarpohu@gmail.com>
2026-07-19 02:51:03 -0400

    Export authority platform modules

---
+++ packages/authority-platform/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.js";
+export * from "./seo.js";
+export * from "./structuredData.js";
+export * from "./linking.js";
+export * from "./quality.js";
+export * from "./triage.js";
